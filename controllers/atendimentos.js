4e214c21f500adecddc89fce5171016d1017c626
joaovictordjardim <joaovictordjardim@gmail.com>
2021-02-05 19:27:15 -0300

    Criando tabelas e conexão com o banco de dados

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,12 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
   app.get('/atendimentos', (req,res) => res.send('Você está na rota de atendimento, fazendo um GET'));
   
   app.post('/atendimentos', (req,res) => {
-    console.log(req.body);
-    res.send('Você está na rota de atendimento, fazendo um POST');
+    // console.log(req.body);
+    const atendimento = req.body;
+    Atendimento.adiciona(atendimento);
+    res.send('POST enviado');
   });
 } 
--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
 
-const app = customExpress();
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+conexao.connect(err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Conectado com sucesso!');
+    
+    Tabelas.init(conexao)
+    
+    const app = customExpress();
+    app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+  }
+});
