becd8c9f2a40997a7826d9071607c9ca9b9bfa84
joaovictordjardim <joaovictordjardim@gmail.com>
2021-02-05 21:27:16 -0300

    Projeto finalizado

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -33,13 +33,64 @@ class Atendimento {
       const sql = 'INSERT INTO Atendimentos SET ?';
 
       conexao.query(sql, atendimentoDatado, (err, resultados) => {
-          if(err) {
-            res.status(400).json(err);
-          } else {
-            res.status(201).json(atendimento);
-          }
-      })
+        if(err) {
+          res.status(400).json(err);
+        } else {
+          res.status(201).json(atendimento);
+        }
+      });
     }
+  }
+
+  lista(res) {
+    const sql = 'SELECT * FROM Atendimentos';
+    conexao.query(sql, (err, resultados) => {
+      if (err) {
+        res.status(400).json(err);
+      } else {
+        res.status(200).json(resultados);
+      }
+    });
+  }
+
+  buscaPorId (id, res) {
+    const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
+
+    conexao.query(sql, (err, resultados) => {
+      const atendimento = resultados[0];
+      if (err) {
+        res.status(400).json(err);
+      } else {
+        res.status(200).json(atendimento);
+      }
+    })
+  }
+
+  altera(id, valores, res) {
+    if (valores.data) {
+      valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+    }
+    const sql = 'UPDATE Atendimentos SET ? WHERE id=?';
+
+    conexao.query(sql, [valores, id], (err, resultados) => {
+      if (err) {
+        res.status(400).json(err);
+      } else {
+        res.status(200).json({...valores, id});
+      }
+    })
+  }
+
+  deleta(id, res) {
+    const sql = 'DELETE FROM Atendimentos WHERE id=?';
+
+    conexao.query(sql, id, (err, resultados) => {
+      if (err) {
+        res.status(400).json(err);
+      } else {
+        res.status(200).json({id});
+      }
+    })
   }
 }
 
